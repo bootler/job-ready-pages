71efa729d69f28f0b72dc95fade91c455cee951b
bootler <aboolean123@hotmail.com>
2019-06-06 02:07:45 -0400

    ResumePage: add smooth scrolling and section highligting

--- Resume page/js/menu.js
+++ Resume page/js/menu.js
@@ -1,7 +1,51 @@
-/* Toggle menu collapse */
+/* Menu.js - Control the behavior of the navigation menu*/
 
-const menu = document.querySelector('.collapsible');
 
-function toggleCollapse() {
-    menu.classList.contains('show') ? menu.classList.remove('show') : menu.classList.add('show');
+const collapsible = document.querySelector('.collapsible');
+const links = document.querySelectorAll(".nav-link");
+const sections = document.querySelectorAll("section");
+
+//add collapse function to each of the links in the nav menu
+window.onload = () => {
+   links.forEach(link => link.addEventListener('click', collapse, false));
+   
 }
+
+window.onscroll = () => {
+    addHighlight();
+}
+
+//collapse the menu by clicking a navigation link within
+function collapse() {
+    collapsible.classList.remove("show");
+    
+}
+
+//toggle the collapse menu with the nav bar button
+function toggleCollapse() {
+    collapsible.classList.contains('show') ? collapsible.classList.remove('show') : collapsible.classList.add('show');
+}
+
+function isInViewport(elem) {
+    const vh = window.innerHeight;
+    const rect = elem.getBoundingClientRect();
+
+    return (rect.top <= vh && rect.bottom >= 0);
+}
+
+function addHighlight() {
+
+    let flag = false;
+    sections.forEach(section => {
+        let query = "a[href='#" + section.id + "']";
+        let link = document.querySelector(query);
+        link.classList.remove("highlighted");
+
+        if (isInViewport(section) && !flag) {
+            link.classList.add("highlighted");
+            flag = true;
+        }
+    });
+}
+
+
